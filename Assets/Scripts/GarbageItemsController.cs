18a029ea0ab4aeb656b1f50c1794de50dae69003
Yurdanur22 <yurdanurciftci4@gmail.com>
2026-05-01 15:51:39 +0300

    Second Scene Added

---
+++ Assets/Scripts/GarbageItemsController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class GarbageItemsController : MonoBehaviour
+{
+    public GameObject missionText;
+    int garbageCount;
+    void Start()
+    {
+        garbageCount = GameObject.FindGameObjectsWithTag("Garbage").Length;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                GameObject clickedObject = hit.collider.gameObject;
+                if(clickedObject.tag == "Garbage")
+                {
+                    clickedObject.SetActive(false);
+                    garbageCount--;
+                    Debug.Log(garbageCount);
+
+                    if (garbageCount <= 1)
+                    {
+                        missionText.SetActive(false);
+                    }
+                }
+                
+            }
+        }
+    }
+
+}
+
